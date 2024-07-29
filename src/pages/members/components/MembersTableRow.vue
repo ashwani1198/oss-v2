799a0c25a0f198ebb29548e94632faf6791b40d9
ashwani1198 <ashwani.singh@anrxsolutions.com>
2024-07-29 21:33:46 +0530

    hovercard and popover for actions

--- src/pages/members/components/MembersTableRow.vue
+++ src/pages/members/components/MembersTableRow.vue
@@ -3,6 +3,7 @@ import { TableCell, TableRow } from '@/components/ui/table'
 import { Button } from '@/components/ui/button'
 import { Ellipsis } from 'lucide-vue-next'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
 
 import { useRouter } from 'vue-router/auto'
 
@@ -14,7 +15,10 @@ const props = defineProps<{
 
 const test = (member: Member) => {
   console.log('test', member)
-  router.push(`/members/${member.id}`)
+}
+
+const viewProfile = (memberId: number) => {
+  router.push(`/members/${memberId}`)
 }
 
 const router = useRouter()
@@ -35,16 +39,60 @@ const router = useRouter()
     <TableCell class="max-w-md p-2">{{ member.renewal_date }} </TableCell>
     <TableCell class="max-w-md p-2 capitalize">{{ member.membership_type }} </TableCell>
     <TableCell>
-      <HoverCard :open-delay="0" :close-delay="0">
-        <HoverCardTrigger>
-          <Button size="icon" variant="ghost">
-            <Ellipsis class="h-6 w-6 text-black" />
-          </Button>
-        </HoverCardTrigger>
-        <HoverCardContent :side="'bottom'" :align="'center'" class="p-1">
-          <Button class="w-full" variant="ghost" @click="test(member)"> EDIT </Button>
-        </HoverCardContent>
-      </HoverCard>
+      <div class="md:block hidden">
+        <HoverCard :open-delay="0" :close-delay="0">
+          <HoverCardTrigger>
+            <Button size="icon" variant="ghost">
+              <Ellipsis class="h-6 w-6 text-black" />
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent :side="'bottom'" :align="'center'" class="p-1 text-left">
+            <Button class="w-full flex justify-start" variant="ghost" @click="test(member)">
+              Edit
+            </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Delete </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Add Notes </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Add Receipt </Button>
+            <Button class="w-full flex justify-start" variant="ghost">
+              Add Document Signing Date
+            </Button>
+            <Button class="w-full flex justify-start" variant="ghost">View Contract </Button>
+            <Button
+              class="w-full flex justify-start"
+              variant="ghost"
+              @click="viewProfile(member.id)"
+              >View Profile
+            </Button>
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div class="md:hidden">
+        <Popover>
+          <PopoverTrigger>
+            <Button size="icon" variant="ghost">
+              <Ellipsis class="h-6 w-6 text-black" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent :align="'center'" :side="'bottom'" class="p-1">
+            <Button class="w-full flex justify-start" variant="ghost" @click="test(member)">
+              Edit
+            </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Delete </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Add Notes </Button>
+            <Button class="w-full flex justify-start" variant="ghost"> Add Receipt </Button>
+            <Button class="w-full flex justify-start" variant="ghost">
+              Add Document Signing Date
+            </Button>
+            <Button class="w-full flex justify-start" variant="ghost">View Contract </Button>
+            <Button
+              class="w-full flex justify-start"
+              variant="ghost"
+              @click="viewProfile(member.id)"
+              >View Profile
+            </Button>
+          </PopoverContent>
+        </Popover>
+      </div>
     </TableCell>
   </TableRow>
 </template>
